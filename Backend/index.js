676a2e4c43141ec1786bc041bb79d573ea8e2342
Garayf <Edgarayf@unal.edu.co>
2022-04-13 17:41:38 -0500

    rename

--- Backend/index.js
+++ Backend/index.js
@@ -10,8 +10,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
   
-  
- 
+   
 app.get('/testdata', (req, res, next) => {
     console.log("TEST endpoint :");    
     res.send("funciona")
